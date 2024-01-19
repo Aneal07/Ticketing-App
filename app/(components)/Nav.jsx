0dc2f393e01f230892d3cdc094e0f97bb995887c
Aneal07 <anilmaharjan@outlook.com>
2024-01-19 16:24:24 +0000

    create status display & progressdisplay

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
         </Link>
         </div>
         <div>
-            <p className='text-default'>@outlook.com</p>
+            <p className='text-default'>something@outlook.com</p>
         </div>
     </nav>
   )
--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-
-const StatusDisplay = () => {
+const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color;
+    switch (status.toLowerCase()) {
+      case "done":
+        color = "bg-green-500";
+        return color;
+      case "started":
+        color = "bg-yellow-500";
+        return color;
+      case "not started":
+        color = "bg-orange-500";
+        return color;
+      default:
+        color = "bg-slate-700";
+    }
+    return color;
+  };
   return (
-   <span className='inline-block rounded-full px-2 py-1 text-xs 
-   font-semi-blod text-gray-700 bg-green-200'>
-    Done
-   </span>
+    <span
+      className={`inline-block  rounded-full px-2 py-1 text-xs font-semibold text-white-500 ${getColor(
+        status
+      )}`}
+    >
+      {status}
+    </span>
+  );
+};
 
-  )
-}
-
-export default StatusDisplay
+export default StatusDisplay;
